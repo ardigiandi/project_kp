eac5a906594cd67eeb8bfad295f65f1421cfbed6
Ardiansyah Giandi <ardiansyahg9080@gmail.com>
2025-03-24 00:25:38 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["index.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        hijau: "#4CAF4F",
+        hitam: "#18191F",
+        abu: " #4D4D4D",
+        background: "#F5F7FA",
+      }
+    },
+  },
+  plugins: [],
+}
